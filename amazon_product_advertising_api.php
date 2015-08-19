a85aa0082f0fa75b38c4522d22b2688ac2433a5c
wade0926 <posipsy@gmail.com>
2015-08-19 17:28:10 +0800

    update to right file

--- amazon_product_advertising_api.php
+++ amazon_product_advertising_api.php
@@ -1,210 +1,279 @@
 <?php
-include_once('../pd/amazon_api.php');
+require_once('../pd/amazon_api.php');
+require_once('func/api_amazon.php');
 
-//選功能
-$which_api_act = 2;
-$item = item_search($which_api_act);
+$act = $_GET['act'];
+$search_index = $_GET['search_index'];
+$asin = $_GET['asin'];
+$page = $_GET['page'];
+
+if($act != '')
+{
+	$arr_api['act'] = $act;		
+		
+	if($arr_api['act'] == 'item_search')
+	{
+		$arr_api['search_index'] = $search_index;
+		$arr_api['page'] = $page;
+		//$arr_api['title'] = '学園';
+		//$arr_api['minimum_price'] = '4000';
+		//$arr_api['maximum_price'] = '5000';
+		$arr_api['sort'] = 'price';
+		$item = amazon_item_search($arr_api);
+	}
+	
+	if($arr_api['act'] == 'item_lookup')
+	{
+		$arr_api['asin'] = $asin;		
+		$item = amazon_item_lookup($arr_api);
+	}	
+}
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<X-Frame-Options: Deny>
-<meta name="title" content="測試頁" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script src="js/jquery.min.js"></script>
-<title>測試頁</title>
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<title>Amazon API測試頁</title>
 </head>
-<body>
-<?php
-//(for test)
-if(0)
-{
-	echo '<pre>';
-	//print_r($item);
-	//print_r($item[1]['BrowseNode']);
-	print_r($item[1]['Item']);
-	echo '</pre>';
-	exit;
-	//var_dump($item[1]['Item'][0]['ItemAttributes']);exit;
-}
+<body style="margin-left:30px;">
+<div style="margin:20px">
+	<form method="get" onsubmit="return check_form();">
+    	<span>
+            請選擇功能：
+            <select name="act" id="act" onchange="act_change();">
+                <option value="">請選擇</option>
+                <option value="item_search">查分類樹下商品</option>
+                <option value="item_lookup">取得商品詳細資訊</option>
+            </select>
+        </span>
+        
+        <span id="span_search_index" style="margin-left:20px;">
+            選擇分類樹：
+            <select name="search_index" id="search_index">
+                <option value="">請選擇</option>
+                <option value="DVD">DVD</option>
+                <option value="Books">Books</option>
+                <option value="adult_anime_DVD">成人動漫DVD</option>
+            </select>
+        </span>
+        
+        <span id="span_page" style="margin-left:20px;">
+        	頁數：
+            <select name="page" style="margin-bottom:20px;">
+                <?php
+                for($i = 1;$i <= 10;$i++)
+                {
+                    ?>
+                    <option value="<?php echo $i;?>"><?php echo $i;?></option>
+                    <?php
+                }
+                ?>            
+            </select>  
+        </span>
+        
+        <span id="span_asin" style="margin-left:20px;">
+            ASIN：
+            <input name="asin" id="asin" />
+        </span>        
+        
+        <span id="btn" style="margin-left:20px;">
+        	<button type="submit">送出</button>
+        </span>
+    </form>
+</div>
 
-switch($which_api_act)
+<?php
+if($act != '')
 {
-	//ItemSearch
-	case 2:
+	//依分類查商品
+	if($act == 'item_search')
+	{
 		?>
         <div style="margin-bottom:20px;">
-            ．共
+        	．共
             <span style="color:red">
-                <?php echo $item[1]['TotalResults'];?>
+				<?php echo $item[1]['TotalResults'];?>
             </span>
             &nbsp;項商品
-        </div>  
+        </div>                      
         
-        <table>
-        <tr>
-            <th>項次</th>
-            <th>圖片</th>
-            <th>ASIN</th>        
-            <th>title</th>
-            <th>價格</th>
-        </tr>   
-        <?php
-        $i = 1;	
-            
-        foreach($item[1]['Item'] as $row)
-        {		
-            ?>
-            <tr>
-                <td><?php echo $i;?></td>
-                <td>
-                    <a href="<?php echo $row['DetailPageURL'];?>" target="_blank">
-                        <img src="<?php echo $row['MediumImage']['URL'];?>">
-                    </a>
-                </td>
-                <td><?php echo $row['ASIN'];?></td>
-                <td>
-                    <a href="<?php echo $row['DetailPageURL'];?>" target="_blank">
-                        <?php echo $row['ItemAttributes']['Title'];?>
-                    </a>
-                </td>                
-                <td><?php echo '￥ '.$row['Offers']['Offer']['OfferListing']['Price']['Amount'];?></td>			
-            </tr>
-            <?php
-            $i++;
-        }
-        ?>
-        </table>
-        <?php
-	break;
-	
-	//ItemLookup
-	case 3:
+		<table>
+			<tr>
+				<th>項次</th>
+				<th>圖片</th>
+				<th>ASIN</th>        
+				<th>title</th>
+				<th>價格</th>
+			</tr>   
+			<?php
+			$i = 1;	
+				
+			foreach($item[1]['Item'] as $row)
+			{		
+				?>
+				<tr>
+					<td><?php echo $i;?></td>
+					<td>
+						<a href="<?php echo $row['DetailPageURL'];?>" target="_blank">
+							<img src="<?php echo $row['MediumImage']['URL'];?>">
+						</a>
+					</td>
+					<td><?php echo $row['ASIN'];?></td>
+					<td>
+						<a href="<?php echo $row['DetailPageURL'];?>" target="_blank">
+							<?php echo $row['ItemAttributes']['Title'];?>
+						</a>
+					</td>                
+					<td><?php echo $row['Offers']['Offer']['OfferListing']['Price']['Amount'].' ￥';?></td>			
+				</tr>
+				<?php
+				$i++;
+			}
+			?>
+		</table>
+		<?php
+	}
+	//查商品詳細資訊
+	elseif($act == 'item_lookup')
+	{	
 		?>
         1. 商品標題：<?php echo $item[1]['Item']['ItemAttributes']['Title'];?>
         <br />
         2. 商品詳情：
+        <div style="margin-left:15px;font-weight:bold">        	
+        	<?php
+            if($item[1]['Item']['ItemAttributes']['Actor'] != '')
+			{
+				?>
+                ．出演：
+                <?php
+				if(is_array($item[1]['Item']['ItemAttributes']['Actor']) == true)
+				{
+					foreach($item[1]['Item']['ItemAttributes']['Actor'] as $row)
+					{
+						$item_detail_actor .= $row.',';				
+					}
+					
+					echo substr($item_detail_actor,0,-1);
+				}
+				else
+				{
+					echo $item[1]['Item']['ItemAttributes']['Actor'];
+				}				
+				?>
+                <br />
+                <?php
+			}
+			
+			if($item[1]['Item']['ItemAttributes']['Actor'] != '')
+			{
+				?>
+                ．監督:
+                <?php
+				if(is_array($item[1]['Item']['ItemAttributes']['Director']) == true)
+				{
+					foreach($item[1]['Item']['ItemAttributes']['Director'] as $row)
+					{
+						$item_detail_director .= $row.',';				
+					}
+					
+					echo substr($item_detail_director,0,-1);
+				}
+				else
+				{
+					echo $item[1]['Item']['ItemAttributes']['Director'];
+				}
+				?>
+                <br />
+                <?php
+			}
+			?>
+            ．DVD區域碼: <?php echo $item[1]['Item']['ItemAttributes']['RegionCode'];?>
+            <br />
+            ．光碟數量： <?php echo $item[1]['Item']['ItemAttributes']['NumberOfDiscs'];?>
+            <br />
+            ．發布日期： <?php echo $item[1]['Item']['ItemAttributes']['ReleaseDate'];?>
+            <br />
+            ．片長： <?php echo $item[1]['Item']['ItemAttributes']['RunningTime']. '分';?>
+            <br />
+            ．內容介紹：
+            <div style="margin-left:15px;color:#F30">
+				<?php
+                //用正規表示式截出商品介紹
+                echo get_product_description($item[1]['Item']['ASIN']);
+                ?>
+            </div>
+        </div>             
+        3. 商品圖片：        
+        <div style="margin-left:15px;">
+            <a href="<?php echo $item[1]['Item']['DetailPageURL'];?>" target="_blank">
+                <img src="<?php echo $item[1]['Item']['LargeImage']['URL'];?>">
+            </a>
+        </div>        
+        4. 商品價格：<?php echo $item[1]['Item']['Offers']['Offer']['OfferListing']['Price']['Amount'].' ￥';?>
         <br />
-        3. 商品圖片：<img src="<?php echo $item[1]['Item']['MediumImage']['URL'];?>">
-        <br />
-        4. 商品價格：<?php echo $item[1]['Item']['Offers']['Offer']['OfferListing']['Price']['Amount'];?>
-        <br />
-        5. 商品庫存：<?php echo $item[1]['Item']['Offers']['Offer']['OfferListing']['Availability'];?>
-        <br />
-        <br />
-        ------ 以下暫時不要 ------
-        <br />
-        ASIN：<?php echo $item[1]['Item']['ASIN'];?>
-        <br />
-        URL：
-        <a href="<?php echo $item[1]['Item']['DetailPageURL'];?>" target="_new">
-            Amazon 商品連結
-        </a>  
-        <br />    
-        
-        product group：<?php echo $item[1]['Item']['ItemAttributes']['ProductGroup'];?>
-        <?php
-	break;
+        5. 商品庫存：<?php echo $item[1]['Item']['Offers']['Offer']['OfferListing']['Availability'];?>        
+		<?php
+	}
+}
+?>
+</body>
+</html>
+
+<script>
+$(function()
+{
+	hide_default()
+});
+
+function hide_default()
+{
+	$('#span_search_index').hide();
+	$('#span_asin').hide();
+	$('#span_page').hide();
+	$('#btn').hide();
 }
 
-//========== function area op ==========
-function item_search($which_api_act)
+function act_change()
 {
-	//AWS Access Key ID
-	$access_key_id = 'AKIAIRPFPHNQRQVJUQWA';
+	hide_default();
 	
-	//AWS Secret Access Key
-	$secret_access_key = secret_access_key;
-	
-	//設定參數
-	$parameters['Service'] = 'AWSECommerceService';
-	$parameters['Version'] = '2011-08-01';
-	$parameters['AssociateTag'] = 'test_tag';
-		
-	//選功能
-	switch($which_api_act)
+	if($('#act').val() == 'item_search')
 	{
-		case 1:
-			$parameters['Operation'] = 'BrowseNodeLookup';		
-			$parameters['BrowseNodeId'] = '2191254051';			
-			$parameters['ResponseGroup'] = 'BrowseNodeInfo';
-		break;
-		
-		case 2:		
-			$parameters['Operation'] = 'ItemSearch';			
-			$parameters['SearchIndex'] = 'DVD';
-			$parameters['BrowseNode'] = '2191254051';		
-			//$parameters['Keywords'] = '巨乳';
-			
-			//--- ResponseGroup ---
-			//$parameters['ResponseGroup'] = 'Small,Images,Offers,OfferFull,OfferSummary';
-			//$parameters['ResponseGroup'] = 'Offers,OfferFull,OfferSummary';
-			$parameters['ResponseGroup'] = 'Large';
-			
-			//--- Condition ---			
-			//$parameters['Condition'] = 'All';
-			//$parameters['Condition'] = 'Used';
-			
-			//$parameters['ItemPage'] = 2;
-			//$parameters['MinimumPrice'] = '3000';
-			//$parameters['MaximumPrice'] = '4000';
-			$parameters['MerchantId'] = 'Amazon';
-						
-			//--- Sort ---
-			$parameters['Sort'] = 'price';
-			
-			//--- 其它 ---
-			//$parameters['Availability'] = 'Available';
-			//$parameters['IncludeReviewsSummary'] = 'true';
-			
-		break;
-		
-		case 3:
-			$parameters['Operation'] = 'ItemLookup';
-			//$parameters['ItemId'] = 'B00F28BU40';			
-			//$parameters['ItemId'] = 'B00F28BU40';
-			//$parameters['ItemId'] = '059035342X';		
-			$parameters['ItemId'] = 'B00WQM3TIK';			
-			//$parameters['ResponseGroup'] = 'Small,Images,OfferFull';
-			$parameters['ResponseGroup'] = 'ItemAttributes';
-		break;
-	}	
-			
-	$parameters['Timestamp'] = gmdate('Y-m-d\TH:i:s\Z');
-	$parameters['AWSAccessKeyId'] = $access_key_id;
-	
-	//對參數陣列排序
-	ksort($parameters);
-	
-	$canonical_string = '';
-	
-	foreach ($parameters as $key => $value)
-	{	
-		$canonical_string .= '&'.str_replace('%7E','~',rawurlencode($key)).'='.str_replace('%7E','~',rawurlencode($value));
+		$('#span_search_index').show();
+		$('#span_page').show();
 	}
 	
-	//去掉第一個& 符號
-	$canonical_string = substr($canonical_string,1);	
-	$string_to_sign = "GET\n"."webservices.amazon.co.jp\n"."/onca/xml\n".$canonical_string;
-	$signature = base64_encode(hash_hmac('sha256',$string_to_sign,$secret_access_key,TRUE));
-	$res_url = 'http://webservices.amazon.co.jp/onca/xml?'.$canonical_string.'&Signature='.str_replace('%7E','~',rawurlencode($signature));	
-	 
-	//(for test)   
-	//echo $res_url;exit;
+	if($('#act').val() == 'item_lookup')
+	{
+		$('#span_asin').show();
+	}
 	
-	$amazon_xml = simplexml_load_string(@file_get_contents($res_url));
-		
-	//利用json功能先把物件轉成陣列
-	foreach($amazon_xml as $value) 
-	{			
-		$res_api[] = json_decode(json_encode($value),true);		
-	}	
-	
-	return $res_api;
+	if($('#act').val() != '')
+	{
+		$('#btn').show();
+	}
 }
-//========== function area ed ==========
-?>
 
-</body>
-</html>   
+function check_form()
+{
+	if($('#act').val() == 'item_search')
+	{
+		if($('#search_index').val() == '')
+		{
+			alert('分類樹未選');
+			return false;
+		}
+	}
+	
+	if($('#act').val() == 'item_lookup')
+	{
+		if($('#asin').val() == '')
+		{
+			alert('ASIN 未填');
+			return false;
+		}
+	}
+}
+</script>   
---
+++ func/api_amazon.php
@@ -0,0 +1,131 @@
+<?php
+//依分類查商品
+function amazon_item_search($arr)
+{
+	$arr['operation'] = 'ItemSearch';	
+	
+	switch($arr['search_index'])
+	{				
+		case 'DVD':
+			$arr['browse_node'] = '561958';					
+		break;
+		
+		case 'Books':
+			$arr['browse_node'] = '465392';
+		break;
+		
+		case 'adult_anime_DVD':
+			$arr['search_index'] = 'DVD';
+			$arr['browse_node'] = '2191254051';					
+		break;				
+	}	
+		
+	return amazon_product_api($arr);
+}
+
+//查商品詳細資訊
+function amazon_item_lookup($arr)
+{
+	$arr['operation'] = 'ItemLookup';
+		
+	return amazon_product_api($arr);
+}
+
+function amazon_product_api($arr)
+{	
+	//========== 設定參數 op ==========
+	//AWS Access Key ID
+	$access_key_id = aws_secret_key_id;
+	
+	//AWS Secret Access Key
+	$secret_access_key = aws_secret_access_key;
+	
+	$parameters['Service'] = 'AWSECommerceService';
+	$parameters['Version'] = '2011-08-01';
+	$parameters['AssociateTag'] = 'ASSOCIATE TAG';	
+	$parameters['Timestamp'] = gmdate('Y-m-d\TH:i:s\Z');
+	$parameters['AWSAccessKeyId'] = $access_key_id;
+	$parameters['Operation'] = $arr['operation'];
+		
+	switch($parameters['Operation'])
+	{	
+		//依分類查商品
+		case 'ItemSearch':					
+			$parameters['SearchIndex'] = $arr['search_index'];
+			$parameters['BrowseNode'] = $arr['browse_node'];
+			$parameters['ItemPage'] = $arr['page'];
+			$parameters['ResponseGroup'] = 'Small,Images,OfferFull';
+			$parameters['MerchantId'] = 'Amazon';
+			
+			//搜尋標題
+			if($arr['title'] != '')
+			{
+				$parameters['Title'] = $arr['title'];
+			}
+			
+			//價格下限
+			if($arr['minimum_price'] != '')
+			{
+				$parameters['MinimumPrice'] = $arr['minimum_price'];
+			}
+			
+			//價格上限
+			if($arr['maximum_price'] != '')
+			{
+				$parameters['MaximumPrice'] = $arr['maximum_price'];
+			}
+			
+			//排序
+			if($arr['sort'] != '')
+			{
+				$parameters['Sort'] = $arr['sort'];
+			}
+		break;
+		
+		//查商品詳細資訊
+		case 'ItemLookup':			
+			$parameters['ItemId'] = $arr['asin'];
+			$parameters['ResponseGroup'] = 'Small,Images,OfferFull,ItemAttributes';			
+		break;
+	}	
+	//========== 設定參數 ed ==========
+			
+	//========== 建立簽名(signature) op ==========	
+	//對參數陣列排序
+	ksort($parameters);
+	
+	$canonical_string = '';
+	
+	foreach ($parameters as $key => $value)
+	{	
+		$canonical_string .= '&'.str_replace('%7E','~',rawurlencode($key)).'='.str_replace('%7E','~',rawurlencode($value));
+	}
+	
+	//去掉第一個& 符號
+	$canonical_string = substr($canonical_string,1);	
+	$string_to_sign = "GET\n"."webservices.amazon.co.jp\n"."/onca/xml\n".$canonical_string;
+	$signature = base64_encode(hash_hmac('sha256',$string_to_sign,$secret_access_key,TRUE));
+	//========== 建立簽名(signature) ed ==========
+		
+	$res_url = 'http://webservices.amazon.co.jp/onca/xml?'.$canonical_string.'&Signature='.str_replace('%7E','~',rawurlencode($signature));
+	$amazon_xml = simplexml_load_string(@file_get_contents($res_url));
+	
+	//利用json功能先把物件轉成陣列
+	foreach($amazon_xml as $value) 
+	{			
+		$res_api[] = json_decode(json_encode($value),true);		
+	}
+		
+	return $res_api;
+}
+
+//用正規表示式截出商品介紹
+function get_product_description($asin)
+{
+	$url = 'http://www.amazon.co.jp/gp/product/black-curtain-redirect.html/375-4630128-1534949?ie=UTF8&redirect=true&redirectUrl=%2Fgp%2Fproduct%2F'.$asin;
+	$res = @file_get_contents($url);	
+	preg_match('/<div class=\"productDescriptionWrapper\">(.*?)<div class=\"emptyClear\">/si',urldecode($res),$res_match);
+		
+	return $res_match[1];
+}
+?>
